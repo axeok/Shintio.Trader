55c14badbee6fdb80b05778bc90807937e911ef8
Aleksey Fedorenko <shintio.afedorenko@gmail.com>
2025-05-17 08:38:10 +0300

    Update benchmark page

--- frontend/src/pages/benchmark5.vue
+++ frontend/src/pages/benchmark5.vue
@@ -1,6 +1,15 @@
 <template>
 	<div class="pa-4">
-		{{ data.Pair }}
+		Show {{ count }} of
+		<span class="text-success">{{ profitable }}</span> wins.
+		Hide <span class="text-error">{{ Object.keys(data.Values).length - profitable }}</span> loses of total
+		{{ Object.keys(data.Values).length }}.
+		<v-slider
+			v-model="count"
+			:min="1"
+			:max="200"
+			:step="1"
+		/>
 		<apexchart
 			width="100%"
 			height="800"
@@ -12,6 +21,8 @@
 
 <script setup>
 	import data from "../../../Shintio.Trader/bin/Debug/net9.0/benchmark.json";
+
+	const count = $ref(20);
 
 	const dates = $computed(() => {
 		const startDate = new Date(data.StartTime);
@@ -28,30 +39,39 @@
 		return result;
 	});
 
-	// const prices = $computed(() => ({
-	// 	name: "Price",
-	// 	type: "candlestick",
-	// 	data: data.Prices.map((value, i) => ({
-	// 		x: dates[i],
-	// 		// x: i,
-	// 		y: value,
-	// 	})),
-	// }));
+	const balances = $computed(() => {
+		const result = (Object.entries(data.Values)
+			.sort((a, b) => {
+				const aLastValue = a[1][a[1].length - 1] ?? 0;
+				const bLastValue = b[1][b[1].length - 1] ?? 0;
+				return bLastValue - aLastValue;
+			})
+			.map((entry, index) => ({
+				name: `#${index + 1} ${entry[0]}`,
+				type: "line",
+				data: entry[1].map((value, i) => ({
+					x: dates[i],
+					y: value,
+				})),
+			})))
+			.filter(s => s.data[0].y < s.data[s.data.length - 1].y);
 
-	const balances = $computed(() => (Object.entries(data.Values)
-		.sort((a, b) => {
-			const aLastValue = a[1][a[1].length - 1] ?? 0;
-			const bLastValue = b[1][b[1].length - 1] ?? 0;
-			return bLastValue - aLastValue;
-		})
-		.map(entry => ({
-			name: entry[0],
-			type: "line",
-			data: entry[1].map((value, i) => ({
-				x: dates[i],
-				y: value,
-			})),
-		}))));
+		return result;
+	});
+
+	const profitable = $computed(() => {
+		let result = 0;
+
+		for (const values of Object.values(data.Values)) {
+			if (values[0] < values[values.length - 1]) {
+				result++;
+			}
+		}
+
+		return result;
+	});
+
+	const total = $computed(() => balances.length);
 
 	const options = $ref({
 		chart: {
@@ -89,7 +109,7 @@
 	const series = $computed(() => {
 		return [
 			// prices,
-			...balances,
+			...evenlySpacedElements(balances, count),
 			// starts,
 			// ends,
 			// deltaBalances,
@@ -97,4 +117,14 @@
 			// winratesSum,
 		];
 	});
+
+	function evenlySpacedElements(arr, count) {
+		if (count <= 1) return [arr[0]];
+		const step = (arr.length - 1) / (count - 1);
+
+		return Array.from({length: count}, (_, i) => {
+			const index = Math.round(i * step);
+			return arr[index];
+		});
+	}
 </script>
